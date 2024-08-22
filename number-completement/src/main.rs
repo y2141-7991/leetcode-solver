1810973414654a0a8809bca6d62a718399334645
Thien Y <y.nguyen@xomad.com>
2024-08-22 12:51:27 +0700

    commit

---
+++ number-completement/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let mut result = String::new();
+    let n: i32 = 1;
+    let bin_num = format!("{:b}", n);
+    for i in bin_num.chars() {
+        if i == '0' {
+            result += "1"
+        }
+        else {
+            result += "0"
+        }
+    }
+    let number =  i32::from_str_radix(&result, 2).unwrap();
+    println!("{}", number)
+
+}
