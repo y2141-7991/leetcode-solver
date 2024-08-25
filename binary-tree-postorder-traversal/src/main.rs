6336f5897e84a7e1460b9ccee5a42734ac1addc3
ynguyen <ndty14@gmail.com>
2024-08-25 16:29:11 +0700

    commit

---
+++ binary-tree-postorder-traversal/src/main.rs
@@ -0,0 +1,41 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+
+struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    fn new(val: i32) -> TreeNode {
+        TreeNode {
+            val,
+            left: None,
+            right: None
+        }
+    }
+}
+
+fn postorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    if let Some(node) = root {
+        let node = node.borrow();
+        let mut ans = self::postorder_traversal(node.left.clone());
+        ans.extend(self::postorder_traversal(node.right.clone()));
+        ans.push(node.val);
+        ans.sort();
+        ans.reverse();
+        ans
+    }
+    else {
+        vec![]
+    }
+}
+
+fn main() {
+    let mut root = TreeNode::new(1);
+    root.left = Some(Rc::new(RefCell::new(TreeNode::new(2))));
+    root.right = Some(Rc::new(RefCell::new(TreeNode::new(3))));
+
+    println!("{:?}", postorder_traversal(Some(Rc::new(RefCell::new(root)))))
+}
