e10f6e60ecd95bd63dbfc767e8f8ec557aca01e4
ynguyen <ndty14@gmail.com>
2024-10-04 22:49:00 +0700

    commit

--- make-sum-divisible-by-p-1590/src/main.rs
+++ make-sum-divisible-by-p-1590/src/main.rs
@@ -1,56 +1,47 @@
-use core::num;
-
 struct Solution;
+
+use std::collections::HashMap;
 
 impl Solution {
     pub fn min_subarray(nums: Vec<i32>, p: i32) -> i32 {
-        for n in 0..nums.len() {
-            let mut cloned_nums = nums.clone();
-            cloned_nums.remove(n);
+        let i64_nums: Vec<i64> = nums.clone().into_iter().map(|a: i32| a as i64).collect();
 
-            let sum_cloned_nums: i32 = cloned_nums.iter().sum();
-            if sum_cloned_nums % p == 0 {
-               return (nums.len() - cloned_nums.len()) as i32
+        let i64_sum_nums: i64 = i64_nums.iter().sum();
+
+        let r = i64_sum_nums % p as i64;
+        if r == 0 {
+            return 0
+        } 
+ 
+        let mut prefix_mod: HashMap<i64, i64> = HashMap::new();
+        prefix_mod.insert(0, -1);
+        let mut prefix_sum: i64 = 0;
+        let mut min_length = nums.len() as i64;
+
+        for (n, m) in nums.iter().enumerate() {
+            prefix_sum += *m as i64;
+            let current_mod = prefix_sum % p as i64;
+            let target_mod = (current_mod as i64 - r + p as i64) % p as i64;
+
+            if prefix_mod.contains_key(&target_mod) {
+                let a = prefix_mod.get(&target_mod).unwrap();
+                min_length = vec![min_length, (n as i64 - a)].into_iter().min().unwrap();
             }
 
-            println!("{:?}", cloned_nums);
-            Self::min_subarray(cloned_nums, p);
-        }
-        let sum_nums: i32 = nums.iter().sum();
-        if sum_nums % p == 0 {
-            return 0
+            prefix_mod.insert(current_mod, n as i64);
+        };
+        println!("{:?}", min_length);
+        if min_length < nums.len() as i64 {
+            return min_length as i32;
         } else {
             -1
         }
-    }
-    fn helpers(nums: Vec<i32>, p: i32, n: usize) -> Vec<i32> {
-        let mut cloned_nums = nums.clone();
-        cloned_nums.remove(n);
-        cloned_nums
-    }
-
-    pub fn min_subarray1(nums: Vec<i32>, p: i32) -> i32 {
-        let sum_nums: i32 = nums.iter().sum();
-        if sum_nums % p == 0 {
-            return 0
-        }
-
-        for n in 0..nums.len() {
-            let a = Self::helpers(nums.clone(), p, n);
-            let das:i32 = a.iter().sum();
-            if das % p == 0 && a.len() > 0{
-                println!("{}", (nums.len() - a.len()) as i32);
-            }
-            Self::min_subarray1(a, p);
-            
-        }
-        
-        -1
     }
 }
 
 fn main() {
     let nums = vec![6,3,5,2];
     let p = 9;
-    println!("{:?}",Solution::min_subarray1(nums, p));
+    println!("{:?}",Solution::min_subarray(nums, p));
+    
 }
