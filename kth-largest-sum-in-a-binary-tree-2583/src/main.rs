582c2c8a6c9ffe8c82661f637e30b5d726e52763
ynguyen <ndty14@gmail.com>
2024-10-22 22:51:22 +0700

    commit

---
+++ kth-largest-sum-in-a-binary-tree-2583/src/main.rs
@@ -0,0 +1,54 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug)]
+struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    fn new(val: i32) -> TreeNode {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+struct Solution;
+
+impl Solution {
+    pub fn kth_largest_level_sum(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
+        let nums: Vec<i32>= Vec::new();
+        fn helpers(nums: Vec<Option<Rc<RefCell<TreeNode>>>>, mut vec_nums: Vec<i32>) -> Vec<i32> {
+            let mut sub_nodes: Vec<Option<Rc<RefCell<TreeNode>>>> = Vec::new();
+            let mut val: i32 = 0;
+            for n in nums {
+                if let Some(n) = n {
+                    let val_num = n.borrow();
+                    val += val_num.val;
+                    sub_nodes.extend(vec![val_num.right.clone(), val_num.left.clone()]);
+                }
+
+            }
+            if val == 0 {
+                return vec_nums;
+            }
+            vec_nums.push(val);
+            helpers(sub_nodes, vec_nums) 
+        }
+        let mut res = helpers(vec![root], nums);
+        res.sort_by(|a, b| b.cmp(a));
+        res[(k as usize) - 1]
+
+    }
+}
+
+fn main() {
+    let mut root = TreeNode::new(312312);
+    root.left = Some(Rc::new(RefCell::new(TreeNode::new(1))));
+    root.right = Some(Rc::new(RefCell::new(TreeNode::new(5))));
+    println!("{:?}", Solution::kth_largest_level_sum(Some(Rc::new(RefCell::new(root))), 1));
+}
