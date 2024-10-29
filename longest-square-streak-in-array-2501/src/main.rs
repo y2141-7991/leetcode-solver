f7d36ec95bbd65bce5c45eca0db9db91d8d8b259
ynguyen <ndty14@gmail.com>
2024-10-29 12:43:01 +0700

    commit

--- longest-square-streak-in-array-2501/src/main.rs
+++ longest-square-streak-in-array-2501/src/main.rs
@@ -2,26 +2,38 @@ struct Solution;
 
 impl Solution {
     pub fn longest_square_streak(nums: Vec<i32>) -> i32 {
-        let mut nums = nums;
-        nums.sort();
-        
-        while !nums.is_empty() {
-            let mut res: Vec<i32> = Vec::new();
-            res.push(nums.remove(0));
-            for i in nums.clone() {
-                if i == nums[nums.len() - 1]*nums[nums.len() - 1] {
-                    
+        const MAX_VALUE: i64 = 100_000;
+        let mut seens = vec![false ; MAX_VALUE as usize + 1];
+        for &num in nums.iter() {
+            seens[num as usize] = true;
+        }
+        let mut longest_streak = 1;
+        for num in nums.into_iter() {
+            let mut current = num as i64;
+            let mut streak = 1;
+            while current * current < MAX_VALUE {
+                let square = current * current;
+                if seens[square as usize] {
+                    streak += 1;
+                    current = square;
+                }
+                else {
+                    break
                 }
             }
-            nums.pop();
+            
+            longest_streak = longest_streak.max(streak);
         }
-        
-        println!("{:?}", nums);
-        0
+        if longest_streak < 2 {
+            -1
+        } else {
+            longest_streak
+        }
     }
 }
 
 fn main() {
-    let a = vec![4,3,6,16,8,2,9];
+    let a = vec![4,3,6,16,8,2,9,81];
     println!("{}", Solution::longest_square_streak(a));
+
 }
