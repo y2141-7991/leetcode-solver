c3a4aaf10e6a327691ae622a52776df71ea479b1
ynguyen <ndty14@gmail.com>
2024-10-31 00:44:43 +0700

    commit

--- minimum-number-of-removals-to-make-mountain-array-1671/src/main.rs
+++ minimum-number-of-removals-to-make-mountain-array-1671/src/main.rs
@@ -1,74 +1,47 @@
+use std::i32;
+
 
 struct Solution;
 
 impl Solution {
+
+    fn lis_length(v: &Vec<i32>) -> Vec<i32> {
+        let mut lis = vec![v[0]];
+        let mut lis_len = vec![1; v.len()];
+
+        for i in 1..v.len() {
+            if v[i] > *lis.last().unwrap() {
+                lis.push(v[i]);
+            }
+            else {
+                let index = lis.partition_point(|&x| x < v[i]);
+                lis[index] = v[i];
+            }
+            lis_len[i] = lis.len() as i32
+        }
+        lis_len
+    }
+
     pub fn minimum_mountain_removals(nums: Vec<i32>) -> i32 {
         let n = nums.len();
-        if n <= 3 {
-            return 0;
-        }
-        let mut res = vec![];
-        for i in 1..n - 1 {
-            if nums[i] >= nums[i-1] && nums[i] >= nums[i+1] {
-                let max_val = nums[i];
-                let idx_max_val = i;
-                
-                let mut dp = vec![false; n];
-                dp[idx_max_val] = true;
-                let dp2 = Self::mapping2(dp.clone(), max_val, idx_max_val, nums.clone());
-                let dp1 = Self::mapping1(dp.clone(), max_val, idx_max_val, nums.clone());
-                let res1 = dp1.iter().filter(|&x| *x == false).count();
-                let res2 = dp2.iter().filter(|&x| *x == false).count();
-                println!("{}, {}, {}", idx_max_val, max_val, res1.min(res2));
-                println!("{:?}", dp1);
-                println!("{:?}", dp2);
-                res.push(res1.min(res2));
+        let lis = Self::lis_length(&nums);
+
+        let mut reversed_nums = nums.clone();
+        reversed_nums.reverse();
+
+        let mut lds = Self::lis_length(&nums);
+        lds.reverse();
+
+        let mut removals = i32::MAX;
+
+        for i in 0..n {
+            if lis[i] > 1 && lds[i] > 1 {
+                removals = removals.min(n as i32 + 1 - lis[i] - lds[i]);
             }
         }
-        res.into_iter().min().unwrap() as i32
+        removals
     }
 
-    fn mapping2(mut dp: Vec<bool>, max_val: i32, idx_max_val: usize, nums: Vec<i32>) -> Vec<bool> {
-        let n = nums.len();
-        let mut cmp_num = max_val;
-        for i in (0..idx_max_val).rev() {
-            if nums[i] < cmp_num as i32 {
-                dp[i] = true;
-                cmp_num = nums[i];
-            }
-            
-        }
-        cmp_num = max_val;
-        for i in idx_max_val+1..n {
-            if nums[i] < cmp_num as i32 {
-                dp[i] = true;
-                cmp_num = nums[i];
-            }
-            
-        }
-        dp
-    }
-    fn mapping1(mut dp: Vec<bool>, max_val: i32, idx_max_val: usize, nums: Vec<i32>) -> Vec<bool> {
-        let n = nums.len();
-        let mut max_num = 0;
-        for i in 0..idx_max_val {
-            if nums[i] < max_val as i32 && nums[i] > max_num {
-                // println!("{}, {}, {}", nums[i], nums[i+1], max_val);
-                dp[i] = true;
-                max_num = nums[i]
-            }
-            
-        }
-
-        let mut min_num = 0;
-        for i in (idx_max_val+1..n).rev() {
-            if nums[i] < max_val as i32 && nums[i] > min_num {
-                dp[i] = true;
-                min_num = nums[i];
-            }
-        }
-        dp
-    }
 }
 
 fn main() {
