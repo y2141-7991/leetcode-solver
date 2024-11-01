3609e0c39f2edcc6973829ed0b610d97e9b848a1
ynguyen <ndty14@gmail.com>
2024-11-01 15:04:26 +0700

    commit

---
+++ delete-characters-to-make-fancy-string-1957/src/main.rs
@@ -0,0 +1,28 @@
+struct Solution;
+
+impl Solution {
+    pub fn make_fancy_string(s: String) -> String {
+        let mut bytestring: Vec<u8> = s.bytes().collect();
+        let mut count = 0;
+        let mut p: u8 = 0;
+        for i in 0..bytestring.len() {
+            
+            if p != bytestring[i] {
+                p = bytestring[i];
+                count = 0;
+            }
+            count += 1;
+            if count > 2 {
+                bytestring[i] = 0;
+            }
+        }
+        bytestring = bytestring.into_iter().filter(|&x| x != 0).collect::<Vec<u8>>();
+        let bytestring = bytestring.into_iter().map(|x| x as char).collect::<Vec<char>>();
+        bytestring.into_iter().collect::<String>()
+    }
+}
+
+fn main() {
+    let a = String::from("leeeeeetcode");
+    println!("{}", Solution::make_fancy_string(a));
+}
