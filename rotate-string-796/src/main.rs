1ea2e7a3925f6f405228375f09a7f1274cea7582
Y <ndty14@gmail.com>
2024-11-03 23:12:15 +0700

    commit

--- rotate-string-796/src/main.rs
+++ rotate-string-796/src/main.rs
@@ -1,3 +1,25 @@
+struct Solution;
+
+impl Solution {
+    pub fn rotate_string(s: String, goal: String) -> bool {
+        let gb: Vec<u8> = goal.bytes().into_iter().collect();
+        let mut vb: Vec<u8> = s.bytes().into_iter().collect();
+        let mut b= vb.len() as i32;
+
+        while b > 0 {
+            let a = vb.remove(0);
+            vb.push(a);
+            if vb == gb {
+                return true;
+            }
+            b -= 1;
+        }
+        false
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a = String::from("abcde");
+    let b = String::from("cdeab");
+    println!("{}", Solution::rotate_string(a, b));
 }
