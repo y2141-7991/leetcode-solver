a9af30fb88a0dbb89ba55db8182a493f7c3067ab
ynguyen <ndty14@gmail.com>
2024-11-12 16:59:43 +0700

    commit

---
+++ most-beautiful-item-for-each-query-2070/src/main.rs
@@ -0,0 +1,15 @@
+struct Solution;
+
+impl Solution {
+    pub fn maximum_beauty(items: Vec<Vec<i32>>, queries: Vec<i32>) -> Vec<i32> {
+        let mut ans: Vec<i32> = vec![];
+        
+        ans
+    }
+}
+
+fn main() {
+    let a = vec![vec![1,2],vec![3,2],vec![2,4],vec![5,6],vec![3,5]];
+    let b = vec![1,2,3,4,5,6];
+    println!("Hello, world!");
+}
